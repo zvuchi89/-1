f6994f14d1c6660d9ee59de50833767ac3400bf5
Ekaterina <zvuchi89@mail.ru>
2022-09-07 07:33:14 +0300

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число : ");
+int k = Convert.ToInt32(Console.ReadLine());
+int max = n; 
+if (n > max) max = n;
+if (i > max) max = i; 
+if (k > max) max = k;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+
