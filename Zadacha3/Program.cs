bed9ace96cf8e2ec0c01feb0548b709f99447dcf
Ekaterina <zvuchi89@mail.ru>
2022-09-07 08:14:50 +0300

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+   Console.Write("Введенное число является четным");
+   Console.Read();
+}
+else
+{
+   Console.Write("Введенное число нечетное");
+   Console.Read();
+}
