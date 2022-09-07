239dab74530a342adf452cdcf8cd4992984dd106
Ekaterina <zvuchi89@mail.ru>
2022-09-07 12:36:03 +0300

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = (-1)*n; i <= n; i++)
+{
+if (i % 2 == 0)
+    {
+    Console.Write ( i );
+    Console.Write (" ");
+    } 
+}
